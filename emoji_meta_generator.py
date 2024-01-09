2ae081160f15dfb4a378a3a5bfe3e40a06a86481
Norm <normandy@biribiri.dev>
2024-01-08 19:06:45 -0500

    generate pleroma-compatible pack.json as well
    Resolves https://codeberg.org/normandy/emoji-meta-generator/issues/1

--- emoji_meta_generator.py
+++ emoji_meta_generator.py
@@ -5,6 +5,7 @@ import datetime
 import pathlib
 import os
 import glob
+from typing import Literal, TypedDict
 
 # TODO: maybe addd *.avif?
 IMAGE_GLOBS = (
@@ -16,7 +17,33 @@ IMAGE_GLOBS = (
     "*.webp",
 )
 
-def generate_meta(files: list[str], category: str) -> dict:
+class MetaEmoji(TypedDict):
+    downloaded: bool
+    fileName: str
+    emoji: TypedDict('emoji', {"name": str, "category": str, "aliases": list})
+
+class Meta(TypedDict):
+    metaVersion: Literal[2]
+    host: str
+    exportedAt: str
+    emojis: list[MetaEmoji]
+
+class Pack(TypedDict):
+    files: dict[str, str]
+    pack: dict
+    count: int
+
+def generate_name_to_file_dict(files: list[str]) -> dict[str, str]:
+    """
+    Return a dictionary that contains a mapping from emoji name to the emoji filename.
+    """
+    result = {}
+    for f in files:
+        path = pathlib.PurePath(f)
+        result[path.stem] = path.name
+    return result
+
+def generate_meta(files: list[str], category: str) -> Meta:
     date = datetime.datetime.now()
     meta = {
         "metaVersion": 2,
@@ -25,13 +52,13 @@ def generate_meta(files: list[str], category: str) -> dict:
     }
     emojis = []
 
-    for f in files:
-        path = pathlib.PurePath(f)
+    files_dict = generate_name_to_file_dict(files)
+    for emoji_name, file_name in files_dict.items():
         emojis.append({
             "downloaded": True,
-            "fileName": path.name,
+            "fileName": file_name,
             "emoji": {
-                "name": path.stem,
+                "name": emoji_name,
                 "category": category,
                 "aliases": []
             }
@@ -39,6 +66,13 @@ def generate_meta(files: list[str], category: str) -> dict:
     
     meta["emojis"] = emojis
     return meta
+
+def generate_pack(files: list[str]) -> Pack:
+    return {
+        "files": generate_name_to_file_dict(files),
+        "pack": {},
+        "count": len(files),
+    }
 
 def main() -> None:
     ap = argparse.ArgumentParser()
@@ -58,6 +92,10 @@ def main() -> None:
     meta = generate_meta(files, category)
     with open(emoji_path / "meta.json", "w") as f:
         json.dump(meta, f)
+    
+    pack = generate_pack(files)
+    with open(emoji_path / "pack.json", "w") as f:
+        json.dump(pack, f)
 
 if __name__ == '__main__':
     main()
